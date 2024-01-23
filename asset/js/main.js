58ba91031a3fbf2a3703519874bbf1723fbae556
unknown <bongtham01@gmail.com>
2024-01-23 21:54:22 +0700

    bong

---
+++ asset/js/main.js
@@ -0,0 +1,11 @@
+import {carousel_banner} from './source.js';
+carousel_banner();
+
+import {slide_img_product} from './source.js';
+slide_img_product();
+
+import {activeBtn_viewMore} from './source.js';
+activeBtn_viewMore();
+
+import {slider_row} from './source.js';
+slider_row('js_dienthoai', 'js_maytinhbang', 'js_dongho', 'js_phukien', 'js_laptop');
